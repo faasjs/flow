81a04bba783bccbad118110de62f9e05e0275795
Ben <ben@zfben.com>
2019-05-23 10:35:26 +0800

    add onMounted

--- src/__tests__/resources.test.ts
+++ src/__tests__/resources.test.ts
@@ -1,49 +1,49 @@
 import Flow from '../index';
 
 describe('resources', function () {
-  // test('unkown', async function () {
-  //   try {
-  //     await new Flow({
-  //       resources: {
-  //         unknow: {},
-  //       },
-  //     }, () => true).createTrigger()({}, {});
-  //   } catch (error) {
-  //     expect(error.message).toEqual('Unknow resource: unknow unknow');
-  //   }
-  // });
+  test('unkown', async function () {
+    try {
+      await new Flow({
+        resources: {
+          unknow: {},
+        },
+      }, () => true).createTrigger()({}, {});
+    } catch (error) {
+      expect(error.message).toEqual('Unknow resource: unknow unknow');
+    }
+  });
 
-  // test('not a function', async function () {
-  //   try {
-  //     await new Flow({
-  //       resources: {
-  //         unknow: {
-  //           handler: 1
-  //         },
-  //       },
-  //     }, () => true).createTrigger()({}, {});
-  //   } catch (error) {
-  //     expect(error.message).toEqual('Resource#unknow<undefined> is not a function');
-  //   }
-  // });
+  test('not a function', async function () {
+    try {
+      await new Flow({
+        resources: {
+          unknow: {
+            handler: 1
+          },
+        },
+      }, () => true).createTrigger()({}, {});
+    } catch (error) {
+      expect(error.message).toEqual('Resource#unknow<undefined> is not a function');
+    }
+  });
 
-  // test('should work', async function () {
-  //   const trigger = new Flow({
-  //     resources: {
-  //       custom: {
-  //         handler: function () {
-  //           return 1;
-  //         }
-  //       }
-  //     }
-  //   }, function () {
-  //     return this.custom;
-  //   }).createTrigger();
+  test('should work', async function () {
+    const trigger = new Flow({
+      resources: {
+        custom: {
+          handler: function () {
+            return 1;
+          }
+        }
+      }
+    }, function () {
+      return this.custom;
+    }).createTrigger();
 
-  //   const res = await trigger({}, {});
+    const res = await trigger({}, {});
 
-  //   expect(res).toEqual(1);
-  // });
+    expect(res).toEqual(1);
+  });
 
   test('read flow when invoke', async function () {
     const trigger = new Flow({
